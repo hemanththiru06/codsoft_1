f5c6e87c42f38fb287e4c3d94f3a0ec19fe41fa9
hemanththiru06 <themanth728@gmail.com>
2024-07-10 12:41:09 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,30 @@
+def calculator(num1,num2):
+    
+    print("Choose an operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    
+    operation = input("Enter the number corresponding to the operation: ")
+    if operation == '1':
+        result = num1 + num2
+        print(f"The result of {num1} + {num2} is: {result}")
+    elif operation == '2':
+        result = num1 - num2
+        print(f"The result of {num1} - {num2} is: {result}")
+    elif operation == '3':
+        result = num2 * num2
+        print(f"The result of {num1} * {num2} is: {result}")
+    elif operation == '4':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result of {num1} / {num2} is: {result}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Invalid operation choice.")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+calculator(num1,num2)
